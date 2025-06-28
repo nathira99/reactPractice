df164ae0f3c0bb1a6131d2dde033b2fe55dbd9fa
nathira99 <nathirafarveen99@gmail.com>
2025-06-28 12:25:34 +0530

    javascript

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -2,11 +2,13 @@ function Greeting() {
 
     let num1 = 5, num2 = 3;
 
+    let add = () => num1 + num2;
+
   return (
     <>
       <h1>Hello, World!</h1>
       <p>This is my first component</p>
-      <p>The sum of {num1} and {num2} is {num1 + num2}</p>
+      <p>The sum of {num1} and {num2} is {add()}</p>
     </>
   );
 }
